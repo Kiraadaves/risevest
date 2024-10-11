4b265fe4e9fef8c5951692b8aecb0297dcd7b45c
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-10-11 22:35:33 +0100

    switching off dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={`${dmSans.className} antialiased`}>
         <ClientLayout>{children}</ClientLayout>
       </body>
